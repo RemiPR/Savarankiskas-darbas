573eabecbbbe083e0eca39c66d385fed77d2ca6f
Remi <prialgauskis.remigijus@gmail.com>
2022-09-21 13:26:55 +0300

    21/09/2022

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,44 @@
+// ----------Fixed NavBar-----------
+
+
+
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+window.onscroll = function () { stickyNav() };
 
 
 const navbar = document.getElementById("nav-wrapper");
 
-const sticky = navbar.offsetTop;
+let sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyNav() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
     }
 }
+
+
+
+
+// ---------------------Fixed go to Top btn--------------------
+
+// let mybutton = document.getElementById("btn-top");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         mybutton.style.display = "block";
+//     } else {
+//         mybutton.style.display = "none";
+//     }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
